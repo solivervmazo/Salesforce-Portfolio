8e3d6042b00dd450d8a96f1a4bb334e2a955fbbd
LAPTOP-Q8PH4MAI\PC <solivervmazo.jobs@gmail.com>
2024-04-23 22:53:30 +0800

    Move static data to lwc

--- force-app/main/default/lwc/pageFooter/pageFooter.js
+++ force-app/main/default/lwc/pageFooter/pageFooter.js
@@ -1,5 +1,26 @@
 import { LightningElement } from "lwc";
-import { FOLLOW_ME } from "c/myUtils";
+
+export const FOLLOW_ME = [
+  {
+    id: 1,
+    name: "LinkedIn",
+    icon: "fa-brands fa-linkedin-in",
+    url: "https://linkedin.com/in/solivervmazo"
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    icon: "fa-brands fa-github",
+    url: "https://github.com/solivervmazo"
+  },
+  {
+    id: 3,
+    name: "Kaggle",
+    icon: "fa-brands fa-kaggle",
+    url: "https://www.kaggle.com/solivermazo"
+  }
+];
+
 export default class PageFooter extends LightningElement {
   socmeds;
 
